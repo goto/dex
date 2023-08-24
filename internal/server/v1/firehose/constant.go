5539aaad34fb7f05e98884879db1378107e12e2b
Stewart Jingga <stewart_jingga@yahoo.com>
2023-08-24 19:45:53 +0700

    feat(firehose): refactor env vars constants

--- internal/server/v1/firehose/constant.go
+++ internal/server/v1/firehose/constant.go
@@ -14,41 +14,13 @@ const (
 	blobSinkType     = "BLOB"
 )
 
-// Firehose Configs used/modified by dex
+// Some of firehose Configs used/modified in more than one place by dex
 // Refer https://goto.github.io/firehose/advance/generic/
 const (
-	configSourceKafkaTopic           = "SOURCE_KAFKA_TOPIC"
-	configSourceKafkaBrokers         = "SOURCE_KAFKA_BROKERS"
-	configSourceKafkaConsumerGroup   = "SOURCE_KAFKA_CONSUMER_GROUP_ID"
-	configSourceKafkaConsumerMode    = "SOURCE_KAFKA_CONSUMER_MODE"
-	configSourceKafkaPollTimeoutMS   = "SOURCE_KAFKA_POLL_TIMEOUT_MS"
-	configSinkType                   = "SINK_TYPE"
-	configStreamName                 = "STREAM_NAME"
-	configProtoClassName             = "INPUT_SCHEMA_PROTO_CLASS"
-	configStencilURL                 = "SCHEMA_REGISTRY_STENCIL_URLS"
-	configStencilEnable              = "SCHEMA_REGISTRY_STENCIL_ENABLE"
-	configJavaOptions                = "_JAVA_OPTIONS"
-	configSinkPoolNumThreads         = "SINK_POOL_NUM_THREADS"
-	configSinkPoolQueuePollTimeoutMS = "SINK_POOL_QUEUE_POLL_TIMEOUT_MS"
-
-	// DLQ
-	configDLQRetryFailAfterMaxAttempEnable = "DLQ_RETRY_FAIL_AFTER_MAX_ATTEMPT_ENABLE"
-	configDLQRetryMaxAttempts              = "DLQ_RETRY_MAX_ATTEMPTS"
-	configDLQSinkEnable                    = "DLQ_SINK_ENABLE"
-	configDLQWriterType                    = "DLQ_WRITER_TYPE"
-	configErrorTypesForDLQ                 = "ERROR_TYPES_FOR_DLQ"
-
-	// Retry
-	configRetryExponentialBackoffInitialMS = "RETRY_EXPONENTIAL_BACKOFF_INITIAL_MS"
-	configErrorTypesForRetry               = "ERROR_TYPES_FOR_RETRY"
-	configRetryMaxAttempts                 = "RETRY_MAX_ATTEMPTS"
-
-	// BIGQUERY
-	configBigqueryTableName               = "SINK_BIGQUERY_TABLE_NAME"
-	configBigqueryTablePartitioningEnable = "SINK_BIGQUERY_TABLE_PARTITIONING_ENABLE"
-	configBigqueryDatasetName             = "SINK_BIGQUERY_DATASET_NAME"
-	configBigqueryRowInsertIDEnable       = "SINK_BIGQUERY_ROW_INSERT_ID_ENABLE"
-	configBigqueryClientReadTimeoutMS     = "SINK_BIGQUERY_CLIENT_READ_TIMEOUT_MS"
-	configBigqueryClientConnectTimeoutMS  = "SINK_BIGQUERY_CLIENT_CONNECT_TIMEOUT_MS"
-	configBigqueryMetadataNamespace       = "SINK_BIGQUERY_METADATA_NAMESPACE"
+	configSourceKafkaTopic         = "SOURCE_KAFKA_TOPIC"
+	configSourceKafkaBrokers       = "SOURCE_KAFKA_BROKERS"
+	configSourceKafkaConsumerGroup = "SOURCE_KAFKA_CONSUMER_GROUP_ID"
+	configSinkType                 = "SINK_TYPE"
+	configStreamName               = "STREAM_NAME"
+	configStencilURL               = "SCHEMA_REGISTRY_STENCIL_URLS"
 )
--- internal/server/v1/firehose/envvars.go
+++ internal/server/v1/firehose/envvars.go
@@ -20,7 +20,7 @@ func (api *firehoseAPI) buildEnvVars(ctx context.Context, firehose *models.Fireh
 		firehose.Configs.EnvVars[configSourceKafkaBrokers] = sourceKafkaBroker
 	}
 
-	firehose.Configs.EnvVars[configStencilEnable] = trueString
+	firehose.Configs.EnvVars["SCHEMA_REGISTRY_STENCIL_ENABLE"] = trueString
 	if fetchStencilURL && firehose.Configs.EnvVars[configStencilURL] == "" {
 		stencilUrls, err := api.getStencilURLs(
 			ctx,
@@ -28,7 +28,7 @@ func (api *firehoseAPI) buildEnvVars(ctx context.Context, firehose *models.Fireh
 			firehose.Configs.EnvVars[configSourceKafkaTopic],
 			streamURN,
 			firehose.Project,
-			firehose.Configs.EnvVars[configProtoClassName],
+			firehose.Configs.EnvVars["INPUT_SCHEMA_PROTO_CLASS"],
 		)
 		if err != nil {
 			return fmt.Errorf("error getting stencil url: %w", err)
@@ -46,35 +46,35 @@ func (api *firehoseAPI) buildEnvVars(ctx context.Context, firehose *models.Fireh
 func buildEnvVarsBySink(sinkType string, envVars map[string]string, cfg *models.FirehoseConfig) map[string]string {
 	// BQ or GCS sink
 	if sinkType == bigquerySinkType || sinkType == blobSinkType {
-		envVars[configJavaOptions] = "-Xmx1550m -Xms1550m"
-		envVars[configDLQRetryFailAfterMaxAttempEnable] = trueString
-		envVars[configDLQRetryMaxAttempts] = "5"
-		envVars[configDLQSinkEnable] = trueString
-		envVars[configDLQWriterType] = "BLOB_STORAGE"
-		envVars[configErrorTypesForDLQ] = "DESERIALIZATION_ERROR,INVALID_MESSAGE_ERROR,UNKNOWN_FIELDS_ERROR,SINK_4XX_ERROR,SINK_5XX_ERROR,SINK_UNKNOWN_ERROR,DEFAULT_ERROR"
-		envVars[configErrorTypesForRetry] = "SINK_5XX_ERROR,SINK_UNKNOWN_ERROR,DEFAULT_ERROR"
-		envVars[configRetryExponentialBackoffInitialMS] = "100"
-		envVars[configRetryMaxAttempts] = "10"
-		envVars[configSourceKafkaPollTimeoutMS] = "60000"
+		envVars["_JAVA_OPTIONS"] = "-Xmx1550m -Xms1550m"
+		envVars["DLQ_RETRY_FAIL_AFTER_MAX_ATTEMPT_ENABLE"] = trueString
+		envVars["RETRY_MAX_ATTEMPTS"] = "5"
+		envVars["DLQ_SINK_ENABLE"] = trueString
+		envVars["DLQ_WRITER_TYPE"] = "BLOB_STORAGE"
+		envVars["ERROR_TYPES_FOR_DLQ"] = "DESERIALIZATION_ERROR,INVALID_MESSAGE_ERROR,UNKNOWN_FIELDS_ERROR,SINK_4XX_ERROR,SINK_5XX_ERROR,SINK_UNKNOWN_ERROR,DEFAULT_ERROR"
+		envVars["ERROR_TYPES_FOR_RETRY"] = "SINK_5XX_ERROR,SINK_UNKNOWN_ERROR,DEFAULT_ERROR"
+		envVars["RETRY_EXPONENTIAL_BACKOFF_INITIAL_MS"] = "100"
+		envVars["RETRY_MAX_ATTEMPTS"] = "10"
+		envVars["SOURCE_KAFKA_POLL_TIMEOUT_MS"] = "60000"
 	}
 
 	// BQ only
 	if sinkType == bigquerySinkType {
-		envVars[configSourceKafkaConsumerMode] = "async"
-		envVars[configSinkPoolNumThreads] = "8"
-		envVars[configSinkPoolQueuePollTimeoutMS] = "100"
-		envVars[configBigqueryTablePartitioningEnable] = trueString
-		envVars[configBigqueryRowInsertIDEnable] = trueString
-		envVars[configBigqueryClientReadTimeoutMS] = "-1"
-		envVars[configBigqueryClientConnectTimeoutMS] = "-1"
-		envVars[configBigqueryMetadataNamespace] = "__kafka_metadata"
-		defaultIfEmpty(envVars, configBigqueryTableName, func() string {
+		envVars["SOURCE_KAFKA_CONSUMER_MODE"] = "async"
+		envVars["SINK_POOL_NUM_THREADS"] = "8"
+		envVars["SINK_POOL_QUEUE_POLL_TIMEOUT_MS"] = "100"
+		envVars["SINK_BIGQUERY_TABLE_PARTITIONING_ENABLE"] = trueString
+		envVars["SINK_BIGQUERY_ROW_INSERT_ID_ENABLE"] = trueString
+		envVars["SINK_BIGQUERY_CLIENT_READ_TIMEOUT_MS"] = "-1"
+		envVars["SINK_BIGQUERY_CLIENT_CONNECT_TIMEOUT_MS"] = "-1"
+		envVars["SINK_BIGQUERY_METADATA_NAMESPACE"] = "__kafka_metadata"
+		defaultIfEmpty(envVars, "SINK_BIGQUERY_TABLE_NAME", func() string {
 			t := envVars[configSourceKafkaTopic]
 			t = strings.ReplaceAll(t, ".", "_")
 			t = strings.ReplaceAll(t, "-", "_")
 			return t
 		})
-		defaultIfEmpty(envVars, configBigqueryDatasetName, func() string {
+		defaultIfEmpty(envVars, "SINK_BIGQUERY_DATASET_NAME", func() string {
 			return *cfg.StreamName
 		})
 	}
