a30fa3e00fbf0cc1db14cbaec0767668384834a7
StewartJingga <stewart_jingga@yahoo.com>
2023-08-31 14:57:52 +0700

    feat(dlq): init dlq package (#60)
    * feat(dlq): init dlq package
    
    * fix: lint warning
    
    * feat(dlq): change routes and package location
    
    * chore: go mod tidy
    
    * feat(dlq): update dlq job model

--- internal/server/server.go
+++ internal/server/server.go
@@ -18,6 +18,7 @@ import (
 	"github.com/goto/dex/internal/server/reqctx"
 	"github.com/goto/dex/internal/server/utils"
 	alertsv1 "github.com/goto/dex/internal/server/v1/alert"
+	dlqv1 "github.com/goto/dex/internal/server/v1/dlq"
 	firehosev1 "github.com/goto/dex/internal/server/v1/firehose"
 	kubernetesv1 "github.com/goto/dex/internal/server/v1/kubernetes"
 	optimusv1 "github.com/goto/dex/internal/server/v1/optimus"
@@ -59,6 +60,7 @@ func Serve(ctx context.Context, addr string,
 		r.Route("/subscriptions", alertsv1.SubscriptionRoutes(sirenClient, shieldClient))
 		r.Route("/optimus", optimusv1.Routes(optimusClient))
 		r.Route("/projects", projectsv1.Routes(shieldClient))
+		r.Route("/dlq", dlqv1.Routes(entropyClient))
 		r.Route("/firehoses", firehosev1.Routes(entropyClient, shieldClient, alertSvc, compassClient, odinAddr, stencilAddr))
 		r.Route("/kubernetes", kubernetesv1.Routes(entropyClient))
 	})
---
+++ internal/server/v1/dlq/handler.go
@@ -0,0 +1,55 @@
+package dlq
+
+import (
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+
+	"github.com/goto/dex/internal/server/utils"
+)
+
+type Handler struct {
+	service *Service
+}
+
+func NewHandler(service *Service) *Handler {
+	return &Handler{service: service}
+}
+
+func (h *Handler) listFirehoseDLQ(w http.ResponseWriter, r *http.Request) {
+	// sample to get firehose urn from route params
+	_ = h.firehoseURN(r)
+
+	utils.WriteJSON(w, http.StatusOK, map[string]interface{}{
+		"dlq_list": []interface{}{},
+	})
+}
+
+func (*Handler) listDlqJobs(w http.ResponseWriter, _ *http.Request) {
+	utils.WriteJSON(w, http.StatusOK, map[string]interface{}{
+		"dlq_jobs": []interface{}{},
+	})
+}
+
+func (*Handler) createDlqJob(w http.ResponseWriter, _ *http.Request) {
+	utils.WriteJSON(w, http.StatusOK, map[string]interface{}{
+		"dlq_job": nil,
+	})
+}
+
+func (h *Handler) getDlqJob(w http.ResponseWriter, r *http.Request) {
+	// sample to get job urn from route params
+	_ = h.jobURN(r)
+
+	utils.WriteJSON(w, http.StatusOK, map[string]interface{}{
+		"dlq_job": nil,
+	})
+}
+
+func (*Handler) firehoseURN(r *http.Request) string {
+	return chi.URLParam(r, "firehose_urn")
+}
+
+func (*Handler) jobURN(r *http.Request) string {
+	return chi.URLParam(r, "job_urn")
+}
