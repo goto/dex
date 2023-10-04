511467f5ff73b7d8ba9e9876da635738bedfa599
Lifosmin Simon <lifosmin.simon@Lifosmin-Simon.local>
2023-10-04 17:09:43 +0700

    feat: added new fields for mapping DlqJob

--- internal/server/v1/dlq/mapper.go
+++ internal/server/v1/dlq/mapper.go
@@ -6,8 +6,8 @@ import (
 	entropyv1beta1 "buf.build/gen/go/gotocompany/proton/protocolbuffers/go/gotocompany/entropy/v1beta1"
 	"google.golang.org/protobuf/types/known/structpb"
 
+	"github.com/go-openapi/strfmt"
 	"github.com/goto/dex/entropy"
-	"github.com/goto/dex/generated/models"
 	"github.com/goto/dex/internal/server/utils"
 )
 
@@ -15,8 +15,66 @@ import (
 // from firehose
 // hardcoded
 // user
+type DlqJob struct {
 
-func mapToEntropyResource(job *models.DlqJob) (*entropyv1beta1.Resource, error) {
+	// batch size
+	BatchSize int64 `json:"batch_size,omitempty"`
+
+	// blob batch
+	BlobBatch int64 `json:"blob_batch,omitempty"`
+
+	// created at
+	// Format: date-time
+	CreatedAt strfmt.DateTime `json:"created_at,omitempty"`
+
+	// created by
+	CreatedBy string `json:"created_by,omitempty"`
+
+	// date
+	// Example: 2012-10-30
+	Date string `json:"date,omitempty"`
+
+	// List of firehose error types, comma separated
+	ErrorTypes string `json:"error_types,omitempty"`
+
+	// num threads
+	NumThreads int64 `json:"num_threads,omitempty"`
+
+	// Shield's project slug
+	Project string `json:"project,omitempty"`
+
+	// replicas
+	Replicas int64 `json:"replicas,omitempty"`
+
+	// resource id
+	ResourceID string `json:"resource_id,omitempty"`
+
+	// resource type
+	// Enum: [firehose]
+	ResourceType string `json:"resource_type,omitempty"`
+
+	// status
+	// Enum: [pending error running stopped]
+	Status string `json:"status,omitempty"`
+
+	// stopped
+	Stopped bool `json:"stopped,omitempty"`
+
+	// topic
+	Topic string `json:"topic,omitempty"`
+
+	// updated at
+	// Format: date-time
+	UpdatedAt strfmt.DateTime `json:"updated_at,omitempty"`
+
+	// updated by
+	UpdatedBy string `json:"updated_by,omitempty"`
+
+	// urn
+	Urn string `json:"urn,omitempty"`
+}
+
+func mapToEntropyResource(job DlqJob) (*entropyv1beta1.Resource, error) {
 	cfgStruct, err := makeConfigStruct(job)
 	if err != nil {
 		return nil, err
@@ -40,12 +98,11 @@ func mapToEntropyResource(job *models.DlqJob) (*entropyv1beta1.Resource, error) 
 	}, nil
 }
 
-func makeConfigStruct(job *models.DlqJob) (*structpb.Value, error) {
+func makeConfigStruct(job DlqJob) (*structpb.Value, error) {
 	return utils.GoValToProtoStruct(entropy.JobConfig{
-		Stopped:   job.Stopped,
 		Replicas:  1,
 		Namespace: "", // same with firehose deployment namespace
-		Name:      buildJobName(job),
+		Name:      "",
 		Containers: []entropy.JobContainer{
 			{
 				Name:            "dlq-job",
@@ -57,15 +114,14 @@ func makeConfigStruct(job *models.DlqJob) (*structpb.Value, error) {
 						Mount: "/etc/secret/gcp",                   // from firehose config
 					},
 				},
-				Limits: &entropy.UsageSpec{
+				Limits: entropy.UsageSpec{
 					CPU:    "0.5", // user
 					Memory: "2gb", // user
 				},
-				Requests: &entropy.UsageSpec{
+				Requests: entropy.UsageSpec{
 					CPU:    "0.5", // user
 					Memory: "2gb", // user
 				},
-				EnvConfigMaps: []string{},
 				EnvVariables: map[string]string{
 					// all firehose ENV_VARS +
 					"DLQ_BATCH_SIZE":     "100",                // user
@@ -77,20 +133,39 @@ func makeConfigStruct(job *models.DlqJob) (*structpb.Value, error) {
 				},
 			},
 			{
-				Name:            "telegraf",
-				Image:           "telegraf:1.18.0-alpine",
-				ImagePullPolicy: "IfNotPresent",
+				Name:  "telegraf",
+				Image: "telegraf:1.18.0-alpine",
 				ConfigMapsVolumes: []entropy.JobConfigMap{ // confirm with streaming for required secrets
 					{
 						Name:  "dlq-processor-telegraf",
 						Mount: "/etc/telegraf",
 					},
 				},
-				EnvConfigMaps: []string{},
 				EnvVariables: map[string]string{
 					// To be updated by streaming
 					"APP_NAME":        "", //
 					"PROMETHEUS_HOST": "", // from app config
+					"DEPLOYMENT_NAME": "deployment-name",
+					"TEAM":            "de",
+					"TOPIC":           "test-topic",
+					"environment":     "production",
+					"organization":    "de",
+					"projectID":       "projectID",
+				},
+				Command: []string{
+					"/bin/bash",
+				},
+				Args: []string{
+					"-c",
+					"telegraf & while [ ! -f /shared/job-finished ]; do sleep 5; done; sleep 20 && exit 0",
+				},
+				Limits: entropy.UsageSpec{
+					CPU:    "100m",  // user
+					Memory: "300Mi", // user
+				},
+				Requests: entropy.UsageSpec{
+					CPU:    "100m",  // user
+					Memory: "300Mi", // user
 				},
 			},
 		},
@@ -112,7 +187,7 @@ func makeConfigStruct(job *models.DlqJob) (*structpb.Value, error) {
 	})
 }
 
-func buildResourceLabels(job *models.DlqJob) map[string]string {
+func buildResourceLabels(job DlqJob) map[string]string {
 	return map[string]string{
 		"firehose": job.ResourceID,
 		"date":     job.Date,
@@ -121,7 +196,7 @@ func buildResourceLabels(job *models.DlqJob) map[string]string {
 	}
 }
 
-func buildEntropyResourceName(job *models.DlqJob) string {
+func buildEntropyResourceName(job DlqJob) string {
 	return fmt.Sprintf(
 		"%s-%s-%s-%s",
 		job.ResourceID,   // firehose urn
@@ -131,7 +206,7 @@ func buildEntropyResourceName(job *models.DlqJob) string {
 	)
 }
 
-func buildJobName(job *models.DlqJob) string {
+func buildJobName(job DlqJob) string {
 	randomKey := "91238192"
 	return fmt.Sprintf(
 		"%s-%s-%s",
