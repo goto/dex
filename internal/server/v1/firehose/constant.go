b5516867959a6430f8cf714a4f61d06ac4539c22
Ayushi Sharma <ayushi.sharma@gojek.com>
2023-10-23 15:01:41 +0530

    fix: fixed lint errors

--- internal/server/v1/firehose/constant.go
+++ internal/server/v1/firehose/constant.go
@@ -17,12 +17,11 @@ const (
 // Some of firehose Configs used/modified in more than one place by dex
 // Refer https://goto.github.io/firehose/advance/generic/
 const (
-	configSourceKafkaTopic         = "SOURCE_KAFKA_TOPIC"
-	configSourceKafkaBrokers       = "SOURCE_KAFKA_BROKERS"
-	configSourceKafkaConsumerGroup = "SOURCE_KAFKA_CONSUMER_GROUP_ID"
-	configSinkType                 = "SINK_TYPE"
-	configStreamName               = "STREAM_NAME"
-	configStencilURL               = "SCHEMA_REGISTRY_STENCIL_URLS"
+	configSourceKafkaTopic   = "SOURCE_KAFKA_TOPIC"
+	configSourceKafkaBrokers = "SOURCE_KAFKA_BROKERS"
+	configSinkType           = "SINK_TYPE"
+	configStreamName         = "STREAM_NAME"
+	configStencilURL         = "SCHEMA_REGISTRY_STENCIL_URLS"
 )
 
 const (
